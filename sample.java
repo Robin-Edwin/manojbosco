33cc04d2e8bcda9271105e47f9a12d304d9985b8
ROBIN EDWIN <you@example.com>
2025-03-27 19:51:56 +0530

    practise commit

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
 
     int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
+    System.out.println("Fibonacci Pro Series till " + n + " terms:");
 
     while (i <= n) {
       System.out.print(firstTerm + ", ");
